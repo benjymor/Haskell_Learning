370d6faa1cfa2e9b221d1d8c1ef10f93e4539d2c
benjymor <benmoran@Bens-MacBook-Pro.local>
2021-06-17 16:49:38 -0300

    Chnaged function doubleDownUs to divide answer by 2

--- First_Haskell_Function.hs
+++ First_Haskell_Function.hs
@@ -2,4 +2,4 @@ doubleUs x y = doubleMe x + doubleMe y
 
 doubleMe x = x + x
 
-doubleDownUs x y = doubleUs x y * 2 -- this is a new function added 16/6/2021
+doubleDownUs x y = doubleUs x y / 2 -- this is a new function added 16/6/2021
